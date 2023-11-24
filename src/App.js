c8efa8b3bb02ad1a8ebbffd412587bc9a600366b
Abraham Kolawole <Abrahamkolawole87@gmail.com>
2023-11-24 15:26:47 +0000

    Create navbar layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "tailwindcss/tailwind.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing";
 import PropertyPage from "./pages/property";
 import SellersPage from "./pages/seller";
@@ -9,21 +9,26 @@ import BookingsPage from "./pages/bookings";
 import LoginPage from "./pages/auth/login";
 import RegisterPage from "./pages/auth/register";
 import NotFoundPage from "./pages/notfound";
+import Navbar from "./layout/Navbar";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="property" element={<PropertyPage />} />
-        <Route path="seller" element={<SellersPage />} />
-        <Route path="buyer" element={<BuyersPage />} />
-        <Route path="property/:propertyId/booking" element={<BookingsPage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="property" element={<PropertyPage />} />
+          <Route path="sellers" element={<SellersPage />} />
+          <Route path="buyers" element={<BuyersPage />} />
+          <Route
+            path="property/:propertyId/booking"
+            element={<BookingsPage />}
+          />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+    </div>
   );
 }
 
---
+++ src/utils/data.js
@@ -0,0 +1,6 @@
+export const navigation = [
+  { name: "Buyers", href: "/buyers", current: true },
+  { name: "Sellers", href: "/sellers", current: false },
+  { name: "Properties", href: "/property", current: false },
+  { name: "Bookings", href: "/bookings", current: false },
+];
